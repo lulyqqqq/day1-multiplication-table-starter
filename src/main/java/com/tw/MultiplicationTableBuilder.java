7d5ff7b43fe8f9fc4201cab57870f9dc64621424
Mason ma <MAMA14@oocl.com>
2024-11-18 17:31:09 +0800

    [Multiplication]<feat>:create isRangeNumber and isGreaterThen function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -5,17 +5,23 @@ public class MultiplicationTableBuilder {
     public static void main(String[] args) {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
         int start = 2;
-        int end = 4;
-        String multiplicationTable = builder.build(start, end);
-        System.out.println(builder.checkNumber(start,end));
-        System.out.println(multiplicationTable);
+        int end = 9;
+        System.out.println(builder.isRangeNumber(start, end));
+        System.out.println(builder.isGreaterThen(start, end));
+
     }
 
-    public String build(int start, int end) {
-        return "";
+    public Boolean isRangeNumber(int start, int end) {
+        return start > 1 && start <= 1000 && end > 1;
     }
 
-    public boolean checkNumber(int start, int end){
-        return start>1 && start <= 1000 && end > 1 && end <= 1000 && start <= end;
+    public Boolean isGreaterThen(int start, int end) {
+        return end <= 1000 && start <= end;
     }
+
+
+
 }
+//2*2=4
+//2*3=6 3*3=9
+//2*4=8 3*4=12 4*4=16
