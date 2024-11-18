a2a9f4e552ad3db5ebddd4ee499b7dc695a07400
Mason ma <MAMA14@oocl.com>
2024-11-18 16:27:11 +0800

    [Multiplication]<feat>:create checkNumber function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -7,11 +7,15 @@ public class MultiplicationTableBuilder {
         int start = 2;
         int end = 4;
         String multiplicationTable = builder.build(start, end);
-
+        System.out.println(builder.checkNumber(start,end));
         System.out.println(multiplicationTable);
     }
 
     public String build(int start, int end) {
         return "";
+    }
+
+    public boolean checkNumber(int start, int end){
+        return start>1 && start <= 1000 && end > 1 && end <= 1000 && start <= end;
     }
 }
